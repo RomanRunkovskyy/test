9126e479214284509a363915d29aa584ff55345a
WhatANiceDay <romanrynik@gmail.com>
2018-09-04 10:54:44 +0300

    add support for chrome

--- web-client/src/test/java/testng/tests/DummyWebClientTest.java
+++ web-client/src/test/java/testng/tests/DummyWebClientTest.java
@@ -7,10 +7,15 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
+import sun.rmi.runtime.Log;
 import testng.base.WebClientTestBase;
 
 public class DummyWebClientTest extends WebClientTestBase {
+
+    private static final Logger log = LoggerFactory.getLogger(WebClientTestBase.class);
 
     private final static String DB_NAME = "TESTDATA (MS-vm)";
     private final static String USERNAME = "TEST_USER";
@@ -32,7 +37,7 @@ public class DummyWebClientTest extends WebClientTestBase {
 
         driver.findElement(By.name("q")).sendKeys("BLALA");
         driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
-
+        log.info("-----------:" + driver.findElement(By.name("q")).getTagName());
 
 
     }
